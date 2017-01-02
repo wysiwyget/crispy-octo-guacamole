0aaf05f99df0903bf148765355a0a4f4b00c152e
wysiwyget <wysiwyg@sigaint.org>
2017-01-02 12:38:07 +0000

    add cool loop

--- main.c
+++ main.c
@@ -5,4 +5,8 @@ int main(int argc, char **argv) {
   printf("this is crispy-octo-guacamole ;)\n\n");
 
   printf("expect more features soon.\n\n");
+
+  for (int i = 0; i < 10; i++) {
+    printf("check out this loop (#%i)!11\n", i);
+  }
 }
