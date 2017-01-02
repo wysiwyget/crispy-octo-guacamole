b3622fe03fd4fbc83c1a4f153303ebf2c64298ee
wysiwyget <wysiwyg@sigaint.org>
2017-01-02 14:01:58 +0000

    use COUNT_TO macro

--- main.c
+++ main.c
@@ -1,3 +1,4 @@
+#include "main.h"
 #include <stdio.h>
 
 int main(int argc, char **argv) {
@@ -9,4 +10,6 @@ int main(int argc, char **argv) {
   for (int i = 0; i < 10; i++) {
     printf("check out this loop (#%i)!11\n", i);
   }
+
+  COUNT_TO(10);
 }
